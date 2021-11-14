96788ab99335ea3fb4d6a563c22a6651dcb9799e
Lubo7 <Lkramoris@gmail.com>
2021-11-14 20:01:16 +0100

    ¨Configuration¨

---
+++ config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    url:  //Cadena de conexión mongo db
+   // "mongodb://root:password;@192.168.100.90:27017/geografiaMongo?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"  
+    "mongodb+srv://Lubo7:Andriska1980@cluster0.hfmri.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+}
